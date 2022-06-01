8937f5743f2d8172cd98c4e739ad2a2b9186735d
Moniksama <74771090+Moniksama@users.noreply.github.com>
2022-06-02 00:08:26 +0300

    Update postit.py

--- postit.py
+++ postit.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flaskext.mysql import MySQL
 
 app = Flask(__name__)
@@ -9,21 +9,85 @@ mysql = MySQL()
 def connectBD():
     return mysql.connect()
 
-def readBD():
+def readBD(SQL_sentence):
     conn = connectBD()
     cursor = conn.cursor()
-    cursor.execute("SELECT * from post_it")
-    return cursor.fetchall()
+    cursor.execute(SQL_sentence)
+    datos = cursor.fetchall()
+    conn.close()
+    return datos
 
 @app.route("/")
 def postit():
     return render_template('postit.html')
 
+@app.route("/postit/list_notes")
+def list_notes():
+    datos = readBD("SELECT * from post_it")
+    return render_template('list_notes.html', data=datos)
 
-@app.route("/postit/listar_notas")
-def listar_notas():
-    datos = readBD()
-    return render_template('listar_notas.html', data=datos)
+@app.route("/postit/add_notes")
+def add_notes():
+    datos = readBD("SELECT * from marca ORDER BY idmarca")
+    return render_template('add_notes.html', data=datos)
+
+@app.route('/postit/save_note', methods=['POST'])
+def save_note():
+    title = request.form["titulo"]
+    description = request.form["descripcion"]
+    date = request.form["fecha"]
+    priority = request.form["prioridad"]
+    mark = request.form["marca"]
+
+    sentence = "INSERT INTO post_it(titulo, descripcion, fecha, prioridad, idmarca) \
+                    VALUES (%s, %s, %s, %s, %s)"
+
+    conn = connectBD()
+    cursor = conn.cursor()
+    cursor.execute(sentence, (title, description, date, priority, mark) )
+    conn.commit()
+    conn.close()
+
+    return redirect(url_for('postit'))
+
+
+@app.route("/postit/find_notes")
+def search_notes():
+    return render_template('find_notes.html')
+
+
+@app.route("/postit/search_result", methods=['POST'])
+def search_result():
+    text = request.form["texto"]
+
+    sentence = "SELECT * FROM post_it WHERE titulo LIKE '%" + text + "%' OR descripcion LIKE '%" + text + "%'"
+    conn = connectBD()
+    cursor = conn.cursor()
+    cursor.execute(sentence)
+    datos = cursor.fetchall()
+    conn.close()
+
+    return render_template('search_result.html', data=datos)
+
+
+@app.route("/postit/delete_notes")
+def delete_notes():
+    datos = readBD("SELECT * from post_it")
+    return render_template('delete_notes.html', data=datos)
+
+
+@app.route("/postit/delete_row", methods=["POST"])
+def delete_row():
+    postit = request.form["button"]
+
+    sentence = "DELETE FROM post_it WHERE idpost_it = %s"
+    conn = connectBD()
+    cursor = conn.cursor()
+    cursor.execute(sentence, (postit, ) )
+    conn.commit()
+    conn.close()
+
+    return redirect(url_for('delete_notes'))
 
 
 if __name__ == "__main__":
